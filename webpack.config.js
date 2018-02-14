9accdf82d9ba4687fcfa683618d4e4a9d0a11fdb
julianleahy <julianleahynz@gmail.com>
2018-02-15 09:13:13 +1300

    define isProduction variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     context: path.join(__dirname, 'src'),
